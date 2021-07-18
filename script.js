8a51ba66c69d618e4bbcf0390d06186775fc3085
Begimai <begimai.akulova@gmail.com>
2021-07-18 03:13:51 +0300

    temp converter app

---
+++ script.js
@@ -0,0 +1,14 @@
+/* eslint-disable indent */
+let celsius = document.querySelector("#celsius"),
+    fahrenheit = document.querySelector("#fahrenheit");
+
+function celToFar() {
+    let output = (parseFloat(celsius.value) * 9 / 5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+    console.log(output);
+}
+function farToCel(){
+    let output=(parseFloat(fahrenheit.value) -32 ) * 5/9;
+    celsius.value=parseFloat(output.toFixed(2));
+    console.log(output);
+}
